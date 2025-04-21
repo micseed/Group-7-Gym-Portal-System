09a7a507850f948c93b6e5da50a9c8a3a7cd8ff8
micseed <micahsedigo200615@gmail.com>
2025-04-22 00:41:07 +0800

    Final

--- Landing Page/login.js
+++ Landing Page/login.js
@@ -10,10 +10,14 @@ document.addEventListener('DOMContentLoaded', function () {
             const usernameInput = this.querySelector('#username');
             const passwordInput = this.querySelector('#password');
 
-            if (usernameInput.value.includes('energizecouch.com')) {
+            if (usernameInput.value === 'coach@energizecouch.com' && passwordInput.value === 'coachpass') {
                 alert('Coach Login Successful!');
-                window.location.href = 'coach.html';
-            } else if (usernameInput.value.includes('energize.com') && !usernameInput.value.includes('energizecouch.com')) {
+                window.location.href = 'coachmain.html';
+            } else if (usernameInput.value === 'admin@energizecouch.com' && passwordInput.value === 'adminpass') {
+                alert('Admin Login Successful!');
+                window.location.href = 'testadmin.html';
+            }
+            else if (usernameInput.value.includes('energize.com') && !usernameInput.value.includes('energizecouch.com')) {
                 alert('Client Login Successful!');
                 window.location.href = 'client.html';
             } else {
