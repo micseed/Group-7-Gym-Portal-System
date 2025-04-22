c0b58117b6c62a1e8552abbebe1aa8c779ee0df7
micseed <micahsedigo200615@gmail.com>
2025-04-22 12:28:26 +0800

    Final

--- Landing Page/login.js
+++ Landing Page/login.js
@@ -10,16 +10,16 @@ document.addEventListener('DOMContentLoaded', function () {
             const usernameInput = this.querySelector('#username');
             const passwordInput = this.querySelector('#password');
 
-            if (usernameInput.value === 'coach@energizecouch.com' && passwordInput.value === 'coachpass') {
+            if (usernameInput.value === 'coach@energizecoach.com' && passwordInput.value === 'coachpass') {
                 alert('Coach Login Successful!');
                 window.location.href = 'coachmain.html';
-            } else if (usernameInput.value === 'admin@energizecouch.com' && passwordInput.value === 'adminpass') {
+            } else if (usernameInput.value === 'admin@energizecoach.com' && passwordInput.value === 'adminpass') {
                 alert('Admin Login Successful!');
                 window.location.href = 'testadmin.html';
             }
             else if (usernameInput.value.includes('energize.com') && !usernameInput.value.includes('energizecouch.com')) {
                 alert('Client Login Successful!');
-                window.location.href = 'client.html';
+                window.location.href = 'testclient.html';
             } else {
                 loginError.textContent = 'Invalid username or password.';
             }
