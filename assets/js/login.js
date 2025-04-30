8813f7b7e34c2f54a84395ff4aad8e1b6e1e4a80
micseed <micahsedigo200615@gmail.com>
2025-05-01 04:02:53 +0800

    Responsive Website

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,6 +16,9 @@ document.addEventListener('DOMContentLoaded', function () {
             } else if (usernameInput.value === 'admin@energizecoach.com' && passwordInput.value === 'adminpass') {
                 alert('Admin Login Successful!');
                 window.location.href = 'testadmin.html';
+            } else if (usernameInput.value === 'guest' && passwordInput.value === 'guestpass') {
+                alert('Guest Login Successful!');
+                window.location.href = 'guest.html';
             }
             else if (usernameInput.value.includes('energize.com') && !usernameInput.value.includes('energizecouch.com')) {
                 alert('Client Login Successful!');
